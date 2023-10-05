466cf2189645730d8c9c5039ce86e324ad7c4ee6
kamelong <kamelong.dev@gmail.com>
2023-10-05 23:54:48 +0900

    #12

--- src/pages/TimeTableEdit/TimeTableEditPage.tsx
+++ src/pages/TimeTableEdit/TimeTableEditPage.tsx
@@ -6,6 +6,10 @@ import {timetableRoutesSelector, timetableSelector} from "../../store";
 import classes from "../TimeTable/TimeTablePage.module.scss";
 import {StationTimeEditView} from "../TimeTable/StationTimeEditView";
 import {AppTitleState} from "../../App";
+import {IonIcon, IonLabel, IonRouterOutlet, IonSegmentButton, IonSegment, IonTabBar, IonTabButton, IonTabs} from "@ionic/react";
+import {Redirect, Route} from "react-router-dom";
+import {library, playCircle, radio, search} from "ionicons/icons";
+import {IonReactRouter} from "@ionic/react-router";
 
 export const TimetableEditPage: React.FC= () => {
     try {
@@ -13,15 +17,63 @@ export const TimetableEditPage: React.FC= () => {
         const timeTableID = Number(searchParams.get("timetable"));
         const setTitle=useSetRecoilState(AppTitleState);
 
-        setTimeout(()=>{setTitle((old)=>"TimeTableEdit");
+        setTimeout(()=>{setTitle((old)=>"時刻表 設定編集");
         },0)
+
+
 
 
         const timeTable=useRecoilValue(timetableSelector(timeTableID));
         return (
-            <div style={{paddingTop:'10px',height:'100%'}}>
+            <IonSegment value="station" style={{boxSizing: 'border-box',padding:'0px 50px'}}>
+                <IonSegmentButton value="station">
+                    <IonLabel>駅追加/削除</IonLabel>
+                </IonSegmentButton>
+                <IonSegmentButton value="route">
+                    <IonLabel>路線追加/削除</IonLabel>
+                </IonSegmentButton>
+            </IonSegment>
 
-            </div>
+            //     <IonReactRouter>
+            //         <IonTabs>
+            //             <IonRouterOutlet>
+            //                 </IonRouterOutlet>
+            // <IonTabBar slot="bottom">
+            //     <IonTabButton tab="home" >
+            //         <IonIcon icon={playCircle} />
+            //         <IonLabel>Listen now</IonLabel>
+            //     </IonTabButton>
+            //
+            //     <IonTabButton tab="radio" >
+            //         <IonIcon icon={radio} />
+            //         <IonLabel>Radio</IonLabel>
+            //     </IonTabButton>
+            //
+            //     <IonTabButton tab="library">
+            //         <IonIcon icon={library} />
+            //         <IonLabel>Library</IonLabel>
+            //     </IonTabButton>
+            //
+            // </IonTabBar>
+            //                  </IonTabs>
+            //             </IonReactRouter>
+        //     <IonReactRouter>
+        //         <IonTabs>
+        //             <IonRouterOutlet>
+        //                 <Redirect exact path="/" to="/home" />
+        //                 {/*
+        //   Use the render method to reduce the number of renders your component will have due to a route change.
+        //
+        //   Use the component prop when your component depends on the RouterComponentProps passed in automatically.
+        // */}
+        //                 <Route path="./home" render={() => <HomePage />} exact={true} />
+        //                 <Route path="./radio" render={() => <RadioPage />} exact={true} />
+        //                 <Route path="./library" render={() => <LibraryPage />} exact={true} />
+        //             </IonRouterOutlet>
+        //
+        //
+        //     </IonTabs>
+        //     </IonReactRouter>
 
         );
     }catch(e){
@@ -30,4 +82,16 @@ export const TimetableEditPage: React.FC= () => {
             <div></div>
         );
     }
+}
+
+
+export const HomePage: React.FC= () =>{
+    return (<div>Home</div>);
+}
+
+export const RadioPage: React.FC= () =>{
+    return (<div>Radio</div>);
+}
+export const LibraryPage: React.FC= () =>{
+    return (<div>Library</div>);
 }
