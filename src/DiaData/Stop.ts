94bf88c772e3050cfb398161fc62cf15dece32f0
KameLong <kamelong.dev@gmail.com>
2024-01-13 00:06:48 +0900

    プロジェクト再作成

--- src/DiaData/Stop.ts
+++
@@ -1,8 +0,0 @@
-// import {Station} from "./Station";
-//
-// export class Stop{
-//     public station:Station;
-//     constructor(station:Station) {
-//         this.station=station;
-//     }
-// }
